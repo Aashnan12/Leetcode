91b21fe831abf1b2f9098c2849147e557e2c380c
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-02-04 07:40:27 +0530

    Time: 3 ms (67.01%), Space: 13.5 MB (10.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,63 +1,32 @@
-int binary_search(vector<int>& nums, int s, int e ,int key)
-{
-    int start = s;
-    int end = e;
-    int mid = start + (end-start)/2;
-
-    while(start<=end)
-    {
-        if(nums[mid]==key)
-        return mid;
-        if(nums[mid]>key)
-        end = mid - 1;
-        else
-        start = mid + 1;
-
-        mid = start + (end-start)/2;
-    }
-    return -1;
-}
-
-int pivot(vector<int>& nums,int n)
-{
-    int start = 0;
-    int end = n - 1;
-    int mid = start + (end-start)/2;
-
-    while(start<end)
-    {
-        if(nums[mid] >= nums[0])
-        start = mid + 1;
-        else
-        end = mid;
-        
-        mid = start + (end-start)/2;
-
-    }
-    return start;
-}
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       int n = nums.size();
-       int pivotx = pivot(nums,n);
-       if(target >= nums[pivotx] && target <= nums[n-1])
-       return binary_search(nums,pivotx,n-1,target);
-       else
-       return binary_search(nums,0,pivotx-1,target);
+        int start = 0;
+        int end = nums.size()-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[start] <= nums[mid]) {
+                if(nums[start] <= target && target < nums[mid]){
+                    end = mid-1;
+                }
+                else {
+                    start = mid+1;
+                }
+            }
+            else {
+                if(nums[mid] < target && target <= nums[end]){
+                    start = mid+1;
+                }
+                else {
+                    end = mid-1;;
+                }
+            }
+        }
+        return -1;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
