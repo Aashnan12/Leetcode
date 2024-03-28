24d4e7c396d2be48dc860bf5353494dd092a240f
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-03-28 13:20:38 +0530

    Time: 8 ms (99.15%) | Memory: 14 MB (13.78%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string str = "";
-        int i = 0;
-        while(s[i] != '\0'){
-            if(str.size() > 0 && s[i]==str.back()){
-                str.pop_back();
+        // string str = "";
+        // int i = 0;
+        // while(s[i] != '\0'){
+        //     if(str.size() > 0 && s[i]==str.back()){
+        //         str.pop_back();
+        //     }
+        //     else{
+        //         str.push_back(s[i]);
+        //     }
+        //     i++;
+        // }
+        // return str;
+        int i=0,j=0;
+        vector<int> count(s.size());
+        while(j<s.size()){
+            s[i] = s[j];
+            count[i]= 1;
+            if(i>0 && s[i-1] == s[i]){
+                count[i] += count[i-1];
             }
-            else{
-                str.push_back(s[i]);
+            if(count[i] == 2){
+                i -= 2;
             }
-            i++;
+            i++,j++;
         }
-        return str;
+        return s.substr(0,i);
     }
 };
