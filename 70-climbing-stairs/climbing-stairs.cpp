67a8bf8fc0cfc317de70a532d34e5029ff0795b5
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-03-30 09:53:20 +0530

    Time: 3 ms (27.77%) | Memory: 7 MB (97.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==0 || n==1)return n;
+        if(n==0 || n==1)return 1;
         int a = 0;
         int b = 1;
         int num = 0;
