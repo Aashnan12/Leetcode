04bc083293303b75eec77856f860bbffb502c89f
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-02 08:00:28 +0530

    Time: 7 ms (48.84%) | Memory: 8.8 MB (71.19%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mapping(string s){
+        char hash[256] = {0},start = 'a';
+        int i=0;
+        while(s[i] != '\0'){
+            if(hash[s[i]] == 0){
+                hash[s[i]] = start;
+                start++;
+            }
+            i++;
+        }
+        string ans = "";
+        i = 0;
+        while(s[i] != '\0'){
+            ans.push_back(hash[s[i]]);
+            i++;
+        }
+        return ans;
+    }
+    bool isIsomorphic(string s, string t) {
+        return mapping(s)==mapping(t)?true:false;
+    }
+};
