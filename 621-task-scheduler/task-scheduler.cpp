e9d0f1fb9c69cbf844910491ce4a691d3156c5d6
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-02 20:50:29 +0530

    Time: 52 ms (52.46%) | Memory: 37.9 MB (86.88%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,33 +1,16 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // unordered_map<char,int>mp;
-        // int count = 0;
-        // for(auto e : tasks)
-        // {
-        //     mp[e]++;
-        //     count = max(count, mp[e]);
-        // }
-        
-        // int ans = (count-1)*(n+1);
-        // for(auto e : mp) if(e.second == count) ans++;
-        // return max((int)tasks.size(), ans);
-
-        vector<int> mp(26,0);
+        int freq[26] = {0};
+        int maxi = INT_MIN;
         for(char &ch : tasks){
-            mp[ch-'A']++;
+            freq[ch-'A']++;
+            maxi = max(freq[ch-'A'],maxi);
         }
-        sort(mp.begin(),mp.end());
-        int maxfreq = mp[25];
-        int gaddhe = maxfreq - 1;
-        int idleslots = n*gaddhe;
-
-        for(int i=24;i>=0;i--){
-            idleslots -= min(mp[i],gaddhe);
+        int ans = (maxi-1)*(n+1);
+        for(int i=0;i<26;i++){
+            if(maxi == freq[i])ans++;
         }
-        if(idleslots > 0){
-            return tasks.size() + idleslots;
-        }
-        return tasks.size();
+        return max(int(tasks.size()),ans);
     }
 };
