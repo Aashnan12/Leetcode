1a3cf690d68b694bd18d98a45d707c9c45092add
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-05 05:28:47 +0530

    Time: 2 ms (67.48%) | Memory: 8.4 MB (62.60%) - LeetSync

--- 968-beautiful-array/beautiful-array.cpp
+++ 968-beautiful-array/beautiful-array.cpp
@@ -4,9 +4,9 @@ public:
         vector<int> res = {1};
         while(res.size() < n){
             vector<int> temp;
-            for(int &i : res) if(i*2-1 <= n) temp.push_back(i*2-1);
-            for(int &i : res) if(i*2 <= n) temp.push_back(i*2);
-            res = temp;
+            for(int &i : res) if(2*i-1 <= n) temp.push_back(2*i-1);
+            for(int &i : res) if(2*i <= n) temp.push_back(2*i);
+            res = temp; 
         }
         return res;
     }
