c3ef9ff429b69a7a7357a5f19e46a7e5d173c1b1
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-07 17:54:24 +0530

    Time: 6 ms (27.06%) | Memory: 11.1 MB (50.08%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int hash[2001] = {0};
-        for(int i : arr){
-            hash[i+1000]++;
-        }
-        sort(begin(hash),end(hash));
-        for(int i=1;i<2001;i++){
-            if(hash[i] && hash[i-1] == hash[i])return false;
+        // Approach 1
+        // int hash[2001] = {0};
+        // for(int i : arr){
+        //     hash[i+1000]++;
+        // }
+        // sort(begin(hash),end(hash));
+        // for(int i=1;i<2001;i++){
+        //     if(hash[i] && hash[i-1] == hash[i])return false;
+        // }
+        // return true;
+
+        // Approach 2
+        unordered_map<int,int> map;
+        unordered_set<int> set;
+        for(int &i:arr) map[i]++;
+        for(auto it : map){
+            if(set.contains(it.second)) return false;
+            set.insert(it.second);
         }
         return true;
     }
