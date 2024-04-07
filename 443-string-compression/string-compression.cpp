c416ec4c616cec0fdab882df82f2b03344d156a5
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-07 16:27:47 +0530

    Time: 4 ms (65.71%) | Memory: 12.5 MB (87.55%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -2,34 +2,31 @@ class Solution {
 public:
     int compress(vector<char>& s) {
         char prev = s[0];
-        int index = 0;
-        int count = 1;
-        for(int i = 1;i<s.size();i++){
-            if(s[i] == prev){
-                count++;
-            }
+        int index=0,count=1,start;
+        for(int i=1;i<s.size();i++){
+            if(s[i] == prev)count++;
             else{
                 s[index++] = prev;
                 if(count > 1){
-                    int start = index;
+                    start = index;
                     while(count){
-                        s[index++] = (count % 10) + '0';
+                        s[index++] = (count%10) + '0';
                         count = count/10;
                     }
-                    reverse(s.begin() + start,s.begin() + index);
+                    reverse(s.begin()+start, s.begin() + index);
                 }
                 prev = s[i];
-                count = 1;
+                count=1;
             }
         }
         s[index++] = prev;
         if(count > 1){
-            int start = index;
+            start = index;
             while(count){
-                s[index++] = (count % 10) + '0';
+                s[index++] = (count%10) + '0';
                 count = count/10;
             }
-            reverse(s.begin() + start,s.begin() + index);
+            reverse(s.begin()+start, s.begin() + index);
         }
         return index;
     }
