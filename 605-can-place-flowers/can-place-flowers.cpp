263a2d8a2c4f995874eea66f0b880a855de51f30
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-07 15:57:58 +0530

    Time: 7 ms (93.20%) | Memory: 22.7 MB (52.82%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& arr, int n) {
-        int size = arr.size(),i=0;
-        if(size==1 && arr[0]==0 && n==1) return true;
-        while(i<size){
-            if(n<=0){
-                return true;
-            }
-            else if(i==0 && arr[i]==0 && i+1<size && arr[i+1]==0){
-                arr[i]=1;
+    bool canPlaceFlowers(vector<int>& A, int n) {
+        int size = A.size();
+        if(size==1 && A[0]==0 && n==1) return true;
+        for(int i=0;i<size;i++){
+            if(n<=0)return true;
+            if(i==0 && A[i]==0 && i+1<size && A[i+1]==0){
+                A[i]=1;
                 n--;
             }
-            else if(i==size-1 && arr[i]==0 && i>0 && arr[i-1] == 0){
-                arr[i]=1;
+            if(i==size-1 && A[i]==0 && i>0 && A[i-1]==0){
+                A[i]=1;
                 n--;
             }
-            else if(i>0 && i+1<size && arr[i]==0 && arr[i-1]==0 && arr[i+1]==0){
-                arr[i]=1;
+            if(i>0 && i+1<size && A[i]==0 && A[i-1]==0 && A[i+1]==0){
+                A[i]=1;
                 n--;
             }
-            i++;
         }
         return n<=0;
     }
