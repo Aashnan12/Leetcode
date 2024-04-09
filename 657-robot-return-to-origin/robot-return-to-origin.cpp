0681653065fe1439a10873c38d5c3b106ffb3a67
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-09 21:06:28 +0530

    Time: 12 ms (42.37%) | Memory: 9.3 MB (35.69%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    bool judgeCircle(string moves) {
-        int hash[4] = {0};
-        for(auto &i : moves){
-            if(i == 'U'){
-                hash[0]++;
-            }
-            else if(i == 'D'){
-                hash[1]++;
-            }
-            else if(i == 'R'){
-                hash[2]++;
-            }
-            else{
-                hash[3]++;
-            }
+    bool judgeCircle(string s) {
+        // int hash[4] = {0};
+        // for(auto &i : moves){
+        //     if(i == 'U'){
+        //         hash[0]++;
+        //     }
+        //     else if(i == 'D'){
+        //         hash[1]++;
+        //     }
+        //     else if(i == 'R'){
+        //         hash[2]++;
+        //     }
+        //     else{
+        //         hash[3]++;
+        //     }
+        // }
+        // return hash[0]==hash[1] && hash[2]==hash[3];
+        int ud=0,lr=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='U') ud++;
+            else if(s[i]=='D') ud--;
+            else if(s[i]=='R') lr++;
+            else lr--;
         }
-        return hash[0]==hash[1] && hash[2]==hash[3];
+        return !ud && !lr;
     }
 };
