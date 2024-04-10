47fdada3466ec349f05fba251a351f33c65c8dba
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-10 21:36:28 +0530

    Time: 3 ms (60.82%) | Memory: 11.8 MB (76.22%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    bool isValid(string s){
-        stack<int> stk;
-        for(char c : s){
-            if(c=='('){
-                stk.push(c);
-            }
-            else{
-                if(stk.empty() || (c==')' && stk.top()!= '(')){
-                    return false;
-                }
-                stk.pop();
-            }
-        }
-        return stk.empty();
-    }
-    void backtracking(vector<string> &result, string &temp,int n){
+    // bool isValid(string s){
+    //     stack<int> stk;
+    //     for(char c : s){
+    //         if(c=='('){
+    //             stk.push(c);
+    //         }
+    //         else{
+    //             if(stk.empty() || (c==')' && stk.top()!= '(')){
+    //                 return false;
+    //             }
+    //             stk.pop();
+    //         }
+    //     }
+    //     return stk.empty();
+    // }
+    void backtracking(vector<string> &result, string &temp,int n,int open,int close){
         if(temp.size() == 2*n){
-            if(isValid(temp))result.push_back(temp);
+            // if(isValid(temp)
+            result.push_back(temp);
             return;
         }
-        temp.push_back('(');
-        backtracking(result,temp,n);
-        temp.pop_back();
+        if(open < n){
+            temp.push_back('(');
+            backtracking(result,temp,n,open+1,close);
+            temp.pop_back();
+        }
 
-        temp.push_back(')');
-        backtracking(result,temp,n);
-        temp.pop_back();
+        if(close < open){
+            temp.push_back(')');
+            backtracking(result,temp,n,open,close+1);
+            temp.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
+        // Better Approach
+        int open=0,close=0;
         vector<string> result;
         string temp = "";
-        backtracking(result,temp,n);
+        backtracking(result,temp,n,open,close);
         return result;
     }
 };
