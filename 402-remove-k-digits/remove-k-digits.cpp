bdadfbc163294a067fb3eb465e4e33f7c8218a46
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-11 11:56:49 +0530

    Time: 6 ms (94.52%) | Memory: 9.7 MB (93.46%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        stack<char> stack;
-        for(char &c : num){
-            while(k>0 && !stack.empty() && c<stack.top()){
-                stack.pop();
+        // stack<char> stack;
+        // for(char &c : num){
+        //     while(k>0 && !stack.empty() && c<stack.top()){
+        //         stack.pop();
+        //         k--;
+        //     }
+        //     stack.push(c);
+        // }
+
+        // while(k>0 && !stack.empty()){
+        //     stack.pop();
+        //     k--;
+        // }
+
+        // string res = "";
+        // while(!stack.empty()){
+        //     res += stack.top();
+        //     stack.pop();
+        // }
+        // reverse(res.begin(),res.end());
+        // int i=0;
+        // while(res[i]=='0' && res[i] != '\0')i++;
+        // cout<<i<<endl;
+        // res = res.substr(i);
+        // if(res.size() == 0){
+        //     res.push_back('0');
+        //     return res;
+        // }
+        // return res;
+
+        // Better Approach
+
+        string s = "";
+        for(char &ch : num){
+            while(s.size() && k>0  && s.back() > ch){
                 k--;
+                s.pop_back();
             }
-            stack.push(c);
-        }
 
-        while(k>0 && !stack.empty()){
-            stack.pop();
-            k--;
+            if(s.size() || ch!='0')s.push_back(ch);
         }
-
-        string res = "";
-        while(!stack.empty()){
-            res += stack.top();
-            stack.pop();
+        while(s.size() && k--){
+            s.pop_back();
         }
-        reverse(res.begin(),res.end());
-        int i=0;
-        while(res[i]=='0' && res[i] != '\0')i++;
-        cout<<i<<endl;
-        res = res.substr(i);
-        if(res.size() == 0){
-            res.push_back('0');
-            return res;
-        }
-        return res;
+        return (s.size()==0)?"0":s;
     }
 };
