5a4b6a785f7663777fcc47311d8e6bc8bb8a3826
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-11 12:36:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (37.45%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -1,13 +1,30 @@
+// class Solution {
+// public:
+//     string removeDigit(string s, char digit) {
+//         int id=0;
+//         for(int i=0;i<s.size();i++){
+//             if(s[i] == digit ){
+//                 id = i;
+//                 if(i+1<s.size() && s[i] < s[i+1])break;
+//             }
+//         }
+//         return s.substr(0,id) + s.substr(id+1);
+//     }
+// };
+
 class Solution {
 public:
-    string removeDigit(string s, char digit) {
-        int id=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == digit ){
-                id = i;
-                if(i+1<s.size() && s[i] < s[i+1])break;
-            }
-        }
-        return s.substr(0,id) + s.substr(id+1);
-    }
+    	string removeDigit(string s, char digit) {
+		string ans="";
+		int n=s.size();
+		for(int i=0;i<n;i++)
+		{
+			if(s[i]==digit)
+			{
+				string temp=s.substr(0,i)+s.substr(i+1,n-i-1);
+				ans=max(temp,ans);
+			}
+		}
+		return ans;
+		}
 };
