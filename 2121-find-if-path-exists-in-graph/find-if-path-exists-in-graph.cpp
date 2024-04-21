137683753cd3aa5086cac93c9127a7d37b3052f4
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-21 08:55:04 +0530

    Time: 1246 ms (18.67%) | Memory: 279.9 MB (5.02%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs(unordered_map<int,vector<int>> &mp,vector<bool> &vis, int src, int des){
+        if(src == des) return true;
+
+        vis[src] = true;
+
+        for(auto &v : mp[src]){
+            if(!vis[v]){
+                if(dfs(mp,vis,v,des)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+
+        unordered_map<int,vector<int>> mp;
+        for(auto i : edges){
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+        }
+        vector<bool> vis(n,false);
+
+        if(dfs(mp,vis,source,destination)){
+            return true;
+        }
+        return false;
+    }
+};
