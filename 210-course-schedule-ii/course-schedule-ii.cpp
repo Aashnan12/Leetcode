aa6756113cd4021bb04f45ceca82cd8e93bfb9b1
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-28 19:29:21 +0530

    Time: 19 ms (38.36%) | Memory: 17.6 MB (44.95%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> map;
+        vector<int> indegree(n,0);
+
+        for(vector<int> &v : prerequisites){
+            map[v[1]].push_back(v[0]);
+        }
+        for(int i=0;i<n;i++){
+            for(int &v : map[i]){
+                indegree[v]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int> topoSort;
+        int courses = 0;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            courses++;
+            topoSort.push_back(u);
+            for(int &v : map[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+        if(courses == n){
+            return topoSort;
+        }
+        return {};
+    }
+};
