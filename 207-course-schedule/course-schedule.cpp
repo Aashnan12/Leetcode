777d1b8fc418e78e1fb6a6cca47bb4259be4632a
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-28 19:21:54 +0530

    Time: 15 ms (71.44%) | Memory: 17.5 MB (48.98%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& adj) {
+        unordered_map<int,vector<int>> map;
+
+        for(vector<int> &v : adj){
+            map[v[1]].push_back(v[0]);
+        }
+
+        vector<int> indegree(n,0);
+
+        for(int i=0;i<n;i++){
+            for(int &v : map[i]){
+                indegree[v]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int courses = 0;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            courses++;
+            for(int &v : map[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+        return courses >= n;
+    }
+};
