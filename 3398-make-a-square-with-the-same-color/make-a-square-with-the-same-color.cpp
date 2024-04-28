81aedebf88d3eaf086e7f0e6a1758a43eccbe46d
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-04-28 14:35:37 +0530

    Time: 8 ms (50.00%) | Memory: 19.4 MB (100.00%) - LeetSync

---
+++ 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canMakeSquare(vector<vector<char>>& grid) {
+        int w=0,b=0;
+
+        //First Box
+        grid[0][0]=='W'?w++:b++;
+        grid[0][1]=='W'?w++:b++;
+        grid[1][0]=='W'?w++:b++;
+        grid[1][1]=='W'?w++:b++;
+        if(w >= 3 || b >= 3) return true;
+        w=0,b=0;
+
+        //Second Box
+        grid[0][1]=='W'?w++:b++;
+        grid[0][2]=='W'?w++:b++;
+        grid[1][1]=='W'?w++:b++;
+        grid[1][2]=='W'?w++:b++;
+        if(w >= 3 || b >= 3) return true;
+        w=0,b=0;
+
+        //Third Box
+        grid[1][0]=='W'?w++:b++;
+        grid[1][1]=='W'?w++:b++;
+        grid[2][0]=='W'?w++:b++;
+        grid[2][1]=='W'?w++:b++;
+        if(w >= 3 || b >= 3) return true;
+        w=0,b=0;
+        //Fourth Box
+        grid[1][1]=='W'?w++:b++;
+        grid[1][2]=='W'?w++:b++;
+        grid[2][1]=='W'?w++:b++;
+        grid[2][2]=='W'?w++:b++;
+        if(w >= 3 || b >= 3) return true;
+
+        return false; //Not Possible
+    }
+};
