1b8e34b2c07950968254c7c764c0f49a65fe88cf
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-04 13:02:42 +0530

    Time: 2 ms (71.85%) | Memory: 8.1 MB (95.59%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -2,25 +2,18 @@ class Solution {
 public:
     string addStrings(string a, string b) {
         string ans = "";
-        int m=a.size() , n=b.size(),sum=0,carry=0;
-        int i=m-1 , j=n-1;
+        int i=a.size()-1, j=b.size()-1,sum=0;
 
-        while(i>=0 && j>=0){ 
-            sum = (a[i--]-'0') + (b[j--]-'0') + carry;
-            ans.push_back(sum%10 + '0');
-            carry = sum/10;
+        while(i>=0 || j>=0 || sum){ 
+            if(i>=0){
+                sum += a[i--] - '0' ;
+            }
+            if(j>=0){
+                sum += b[j--] - '0';
+            }
+            ans += sum%10 + '0';
+            sum = sum/10;
         }
-        while(i>=0){ 
-            sum = (a[i--]-'0') + carry;
-            ans.push_back(sum%10 + '0');
-            carry = sum/10;
-        }
-        while(j>=0){ 
-            sum = (b[j--]-'0') + carry;
-            ans.push_back(sum%10 + '0');
-            carry = sum/10;
-        }
-        if(carry) ans.push_back('1');
         reverse(ans.begin(),ans.end());
         return ans;
     }
