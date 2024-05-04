e1fed9386557a4546dbaf202eac555f1c01656ca
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-04 09:18:06 +0530

    Time: 44 ms (96.21%) | Memory: 45.4 MB (85.44%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,3 +1,11 @@
+#pragma GCC optimize("Ofast")
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
