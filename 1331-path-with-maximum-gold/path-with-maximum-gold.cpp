147e83ad418da9e4a107d0ffc75363c6672437fa
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-17 20:02:54 +0530

    Time: 134 ms (62.16%) | Memory: 9 MB (69.69%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -11,39 +11,36 @@ const int ZERO = []()
 
 class Solution {
 public:
-    vector<vector<int>> directions {{-1,0}, {1,0} , {0,-1},{0,1}};
-    int dfs(vector<vector<int>> &grid,int i,int j,int m,int n){
-        if(i>=m || i<0 || j>=n || j<0 || grid[i][j]==0){
+    vector<int> dx{-1, 0, 1, 0};
+    vector<int> dy{0, -1, 0, 1};
+
+    int dfs(vector<vector<int>>& grid, int x, int y, int m, int n) {
+        if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0)
             return 0;
+
+        int originalValue = grid[x][y];
+        grid[x][y] = 0;
+        int maxGold = 0;
+
+        for (int dir = 0; dir < 4; ++dir) {
+            int newX = x + dx[dir], newY = y + dy[dir];
+            maxGold = max(maxGold, dfs(grid, newX, newY, m, n));
         }
 
-        int originalvalue = grid[i][j];
-        grid[i][j] = 0;
-        int maxgold = 0;
-
-        for(auto& dir : directions){
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-
-            maxgold = max(maxgold, dfs(grid,new_i,new_j,m,n));
-        }
-
-        grid[i][j] = originalvalue;
-        return originalvalue + maxgold;
+        grid[x][y] = originalValue;
+        return originalValue + maxGold;
     }
+
     int getMaximumGold(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m = grid.size(), n = grid[0].size();
+        int maxGold = 0;
 
-        int maxgold = 0;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] != 0){
-                    maxgold = max(maxgold, dfs(grid,i,j,m,n));
-                }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j]!= 0)
+                    maxGold = max(maxGold, dfs(grid, i, j, m, n));
             }
         }
-        return maxgold;
+        return maxGold;
     }
 };
