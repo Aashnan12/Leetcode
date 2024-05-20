229907a97b666982d79b644aec48d93e6a178c7e
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-20 18:34:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (98.56%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
-    void solve(vector<int> &nums,int i,vector<int> &currsubset,vector<vector<int>> &subsets){
+    int solve(vector<int> &nums,int i,int Xor){
         if(i == nums.size()){
-            subsets.push_back(currsubset);
-            return;
+            return Xor;
         }
         
-        currsubset.push_back(nums[i]);
-        solve(nums,i+1,currsubset,subsets);
-        currsubset.pop_back();
-        solve(nums,i+1,currsubset,subsets);
+        
+        int include = solve(nums,i+1,nums[i] ^ Xor);
+        
+        int exclude = solve(nums,i+1,Xor);
+
+        return include + exclude;
     }
     int subsetXORSum(vector<int>& nums) {
-        int maxi = 0;
-        vector<vector<int>> subsets;
-        vector<int> currsubset;
-
-        solve(nums,0,currsubset,subsets);
-
-        for(vector<int> &subset : subsets){
-            int Xor = 0;
-            for(int &i : subset){
-                Xor = Xor^i;
-            }
-            maxi += Xor;
-        }
-        return maxi;
+        return solve(nums,0,0);
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     void solve(vector<int> &nums,int i,vector<int> &currsubset,vector<vector<int>> &subsets){
+//         if(i == nums.size()){
+//             subsets.push_back(currsubset);
+//             return;
+//         }
+        
+//         currsubset.push_back(nums[i]);
+//         solve(nums,i+1,currsubset,subsets);
+//         currsubset.pop_back();
+//         solve(nums,i+1,currsubset,subsets);
+//     }
+//     int subsetXORSum(vector<int>& nums) {
+//         int maxi = 0;
+//         vector<vector<int>> subsets;
+//         vector<int> currsubset;
+
+//         solve(nums,0,currsubset,subsets);
+
+//         for(vector<int> &subset : subsets){
+//             int Xor = 0;
+//             for(int &i : subset){
+//                 Xor = Xor^i;
+//             }
+//             maxi += Xor;
+//         }
+//         return maxi;
+//     }
+// };
