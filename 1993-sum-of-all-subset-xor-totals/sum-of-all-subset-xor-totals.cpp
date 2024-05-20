4a974795f15e216c762bec6a5622976d94e6aed1
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-20 18:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (65.34%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    int solve(vector<int> &nums,int i,int Xor){
-        if(i == nums.size()){
-            return Xor;
-        }
-        
-        
-        int include = solve(nums,i+1,nums[i] ^ Xor);
-        
-        int exclude = solve(nums,i+1,Xor);
-
-        return include + exclude;
-    }
     int subsetXORSum(vector<int>& nums) {
-        return solve(nums,0,0);
+        int Xor = 0;
+
+        for(int i=0;i<nums.size();i++){
+            Xor |= nums[i];
+        }
+
+        Xor = Xor << (nums.size()-1);
+        return Xor;
     }
 };
+
+
+// class Solution {
+// public:
+//     int solve(vector<int> &nums,int i,int Xor){
+//         if(i == nums.size()){
+//             return Xor;
+//         }
+        
+        
+//         int include = solve(nums,i+1,nums[i] ^ Xor);
+        
+//         int exclude = solve(nums,i+1,Xor);
+
+//         return include + exclude;
+//     }
+//     int subsetXORSum(vector<int>& nums) {
+//         return solve(nums,0,0);
+//     }
+// };
 
 
 
