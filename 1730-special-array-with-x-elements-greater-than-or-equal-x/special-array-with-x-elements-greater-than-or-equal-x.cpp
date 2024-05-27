479a836255b57e66f0bd179a0f1001818f9b6039
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-27 09:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (9.22%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,19 +1,41 @@
+// Space optimized Approach
 class Solution {
 public:
     int specialArray(vector<int>& nums) {
-        int bucket[1001] = {0};
         int n = nums.size();
+        vector<int> bucket(n+1,0);
 
         for(int i=0;i<n;i++){
-            bucket[nums[i]]++;
+            bucket[min(n,nums[i])]++;
         }
 
-        for(int i=0;i<1001;i++){
-            if(i == n){
+        int cnt = 0;
+        for(int i=n;i>=1;i--){
+            cnt += bucket[i];
+            if(i == cnt){
                 return i;
             }
-            n = n - bucket[i];
         }
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int specialArray(vector<int>& nums) {
+//         int bucket[1001] = {0};
+//         int n = nums.size();
+
+//         for(int i=0;i<n;i++){
+//             bucket[nums[i]]++;
+//         }
+
+//         for(int i=0;i<1001;i++){
+//             if(i == n){
+//                 return i;
+//             }
+//             n = n - bucket[i];
+//         }
+//         return -1;
+//     }
+// };
