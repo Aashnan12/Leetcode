d95a94e81d0415643008d5bce7b598ddf7d99c7e
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-31 02:46:49 +0530

    Time: 61 ms (78.46%) | Memory: 45 MB (71.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,16 +1,38 @@
+// Optimized Solution.
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ans = 0, n = nums.size();
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                if(sum == k){
-                    ans++;
-                }
+        unordered_map<int,int> map;
+        int sum = 0,ans = 0;
+        map[0] = 1; // Edge Case in [1,-1].
+        for(int i=0;i<nums.size();i++){
+            sum += nums[i];
+            if(map.find(sum - k) != map.end()){
+                ans += map[sum - k];
             }
+            map[sum]++;
         }
         return ans;
     }
 };
+
+
+
+
+// Brute Force O(n^2) -> Constraints bekar the isiliye jyada socha ni.
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int ans = 0, n = nums.size();
+//         for(int i=0;i<n;i++){
+//             int sum = 0;
+//             for(int j=i;j<n;j++){
+//                 sum += nums[j];
+//                 if(sum == k){
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
