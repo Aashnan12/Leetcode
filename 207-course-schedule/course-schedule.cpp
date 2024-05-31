076c1e6eacfc8789128133276e8463ca56439498
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-05-31 14:21:25 +0530

    Time: 11 ms (92.35%) | Memory: 18.9 MB (16.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,14 +1,12 @@
-#pragma GCC optimize("Ofast")
-static auto _ = [] () {ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
-
+//Using DFS
 class Solution {
 public:
-    bool isCycle(unordered_map<int,vector<int>> &map,int u,vector<bool> &vis,vector<bool> &inRecursion){
+    bool isCycle(int u,unordered_map<int,vector<int>>& map,vector<bool>& vis,vector<bool>& inRecursion){
         vis[u] = true;
         inRecursion[u] = true;
 
         for(int &v : map[u]){
-            if(!vis[v] && isCycle(map,v,vis,inRecursion)){
+            if(!vis[v] && isCycle(v,map,vis,inRecursion)){
                 return true;
             }
             else if(inRecursion[v]){
@@ -18,18 +16,18 @@ public:
         inRecursion[u] = false;
         return false;
     }
-    bool canFinish(int n, vector<vector<int>>& adj) {
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>> map;
-
-        for(vector<int> &v : adj){
+        for(vector<int>& v : prerequisites){
             map[v[1]].push_back(v[0]);
         }
 
-        vector<bool> inRecursion(n,0);
-        vector<bool> vis(n,0);
+        vector<bool> inRecursion(n+1,false);
+        vector<bool> vis(n+1,false);
+
         for(int i=0;i<n;i++){
-            if(!vis[i] && isCycle(map,i,vis,inRecursion)){
-                return false;
+            if(!vis[i] && isCycle(i,map,vis,inRecursion)){
+                return false; // If cycle , cannot complete courses.
             }
         }
         return true;
@@ -37,44 +35,10 @@ public:
 };
 
 
+// Using BFS
 // class Solution {
 // public:
-//     bool canFinish(int n, vector<vector<int>>& adj) {
-//         unordered_map<int,vector<int>> map;
-
-//         for(vector<int> &v : adj){
-//             map[v[1]].push_back(v[0]);
-//         }
-
-//         vector<int> indegree(n,0);
-
-//         for(int i=0;i<n;i++){
-//             for(int &v : map[i]){
-//                 indegree[v]++;
-//             }
-//         }
-
-//         queue<int> q;
-
-//         for(int i=0;i<n;i++){
-//             if(indegree[i] == 0){
-//                 q.push(i);
-//             }
-//         }
-
-//         int courses = 0;
-
-//         while(!q.empty()){
-//             int u = q.front();
-//             q.pop();
-//             courses++;
-//             for(int &v : map[u]){
-//                 indegree[v]--;
-//                 if(indegree[v] == 0){
-//                     q.push(v);
-//                 }
-//             }
-//         }
-//         return courses >= n;
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
 //     }
 // };
