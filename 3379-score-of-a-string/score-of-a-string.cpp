7434f055ca1099ba8aa264cf0b20c5a0e2869798
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-01 09:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (42.17%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int scoreOfString(string s) {
         int score = 0;
         for(int i=1;i<s.size();i++){
-            score += abs(s[i-1]-s[i]);
+            score += abs(s[i]-s[i-1]);
         }
         return score;
     }
