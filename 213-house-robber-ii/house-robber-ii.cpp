464a4bcba3a6bcc2ca5754e3ed1ce7b4aa58a66e
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-03 13:43:53 +0530

    Time: 4 ms (32.92%) | Memory: 9.2 MB (67.29%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int t[1001];
+    int solve(int idx,int n,vector<int>& nums){
+        if(idx >= n){
+            return 0;
+        }
+        if(t[idx] != -1){
+            return t[idx];
+        }
+        int take = nums[idx] + solve(idx+2,n,nums);
+        int skip = 0 + solve(idx+1,n,nums);
+        return t[idx] = max(take,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        if(n == 2){
+            return max(nums[0],nums[1]);
+        }
+        memset(t,-1,sizeof(t));
+        int first_case = solve(0,n-1,nums);
+        memset(t,-1,sizeof(t));
+        int second_case = solve(1,n,nums);
+        return max(first_case,second_case);
+    }
+};
