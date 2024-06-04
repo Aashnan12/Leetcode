129858924543c90a0fa2370ea7b6c510d9e9755d
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-04 11:27:31 +0530

    Time: 30 ms (58.28%) | Memory: 12.4 MB (91.02%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int t[10010];
+    int t[10001];
     int solve(int amt,vector<int>& coins){
         if(amt == 0){
             return 0;
@@ -10,15 +10,14 @@ public:
         }
         int ans = INT_MAX;
         for(int coin : coins){
-            if(amt - coin >= 0){
-                ans = min(ans + 0LL ,solve(amt-coin,coins) + 1LL);
-            }
+            if(amt - coin >= 0)
+            ans = min(ans + 0LL,solve(amt-coin,coins) + 1LL);
         }
         return t[amt] = ans;
     }
     int coinChange(vector<int>& coins, int amount) {
         memset(t,-1,sizeof(t));
-        int ans = solve(amount,coins);
+        int ans =  solve(amount,coins);   
         return ans == INT_MAX ? -1 : ans;
     }
 };
