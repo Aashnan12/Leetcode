c2e10eda9b40e3b344493158732e814f9a27691a
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-04 12:34:23 +0530

    Time: 45 ms (85.86%) | Memory: 37.9 MB (75.40%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        int hash[26] = {0},maxi,count;
-        for(char &ch : tasks){
-            hash[ch-'A']++;
-            maxi = max(maxi,hash[ch-'A']);
+    int leastInterval(vector<char>& A, int n) {
+        int hash[26] = {0};
+        int maxi = 0;
+        for(int i=0;i<A.size();i++){
+            hash[A[i] - 'A']++;
+            maxi = max(maxi,hash[A[i] - 'A']);
         }
-        count = (maxi-1)*(n+1);
+
+        int count = (maxi-1)*(n+1);
+
         for(int i=0;i<26;i++){
             if(hash[i] == maxi){
                 count++;
             }
         }
-        return max(count,(int)tasks.size());
+        return max(count,(int)A.size());
     }
 };
