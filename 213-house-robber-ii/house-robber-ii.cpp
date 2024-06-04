0ba2eee5e3c7294d6411d6a5e8c292fc063f944c
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-04 11:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (84.66%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int t[1001];
-    int solve(int idx,int n,vector<int>& nums){
-        if(idx >= n){
+    int solve(int i,int n,vector<int>& nums){
+        if(i >= n){
             return 0;
         }
-        if(t[idx] != -1){
-            return t[idx];
+        if(t[i] != -1){
+            return t[i];
         }
-        int take = nums[idx] + solve(idx+2,n,nums);
-        int skip = 0 + solve(idx+1,n,nums);
-        return t[idx] = max(take,skip);
+        int take = solve(i+2,n,nums) + nums[i];
+        int skip = solve(i+1,n,nums);
+
+        return t[i] = max(take,skip);
     }
     int rob(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        memset(t,-1,sizeof(t));
         int n = nums.size();
-        if(n == 1){
-            return nums[0];
-        }
+        int ans = solve(0,n-1,nums);
         memset(t,-1,sizeof(t));
-        int first_case = solve(0,n-1,nums);
-        memset(t,-1,sizeof(t));
-        int second_case = solve(1,n,nums);
-        return max(first_case,second_case);
+        return max(ans,solve(1,n,nums));
     }
 };
