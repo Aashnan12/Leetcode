a931ea7d5ff1d421490cf0dec7db9ea15722805c
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-04 12:22:51 +0530

    Time: 11 ms (69.60%) | Memory: 22.8 MB (15.06%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& A, int n) {
-        int size = A.size();
-        if(size==1 && A[0]==0 && n==1) return true;
+    bool canPlaceFlowers(vector<int>& a, int n) {
+        int count = 0;
+        int size = a.size();
+        int i=0;
+        if(size == 1) return a[0]==0 ||n==0;
         for(int i=0;i<size;i++){
-            if(n<=0)return true;
-            if(i==0 && A[i]==0 && i+1<size && A[i+1]==0){
-                A[i]=1;
+            if(n<=0) return true;
+            if(i==0 && a[i]==0 && i+1 < size && a[i+1]==0){
+                a[i] = 1;
                 n--;
             }
-            if(i==size-1 && A[i]==0 && i>0 && A[i-1]==0){
-                A[i]=1;
+            if(i==size-1 && a[i]==0 && i-1 >=0 && a[i-1]==0){
+                a[i] = 1;
                 n--;
             }
-            if(i>0 && i+1<size && A[i]==0 && A[i-1]==0 && A[i+1]==0){
-                A[i]=1;
+            if(i>0 && i+1 < size && a[i]==0 && a[i-1]==0 && a[i+1]==0){
+                a[i] = 1;
                 n--;
             }
         }
