469e1c64268fa54219cf097218402d66fdcf6d18
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-14 08:23:08 +0530

    Time: 123 ms (49.61%) | Memory: 69.2 MB (24.61%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -5,8 +5,7 @@ public:
         int mini = 0;
         for(int i=1;i<nums.size();i++){
             if(nums[i] <= nums[i-1]){
-                int change = 
-                abs(nums[i] - nums[i-1]) + 1;
+                int change = abs(nums[i] - nums[i-1]) + 1;
                 mini += change;
                 nums[i] += change;
             }
