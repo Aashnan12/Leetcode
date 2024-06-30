2ba16f29d540013a9c21b5d1854f6fbfcbc30f4e
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-06-30 09:35:08 +0530

    Time: 138 ms (54.16%) | Memory: 64.7 MB (85.78%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> res(n);
-        vector<vector<int>> graph(n);
-        
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            vector<bool> visit(n, false);
-            dfs(graph, i, i, res, visit);
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            sort(res[i].begin(), res[i].end());
-        }
-        
-        return res;
-    }
-
-private:
-    void dfs(vector<vector<int>>& graph, int parent, int curr, vector<vector<int>>& res, vector<bool>& visit) {
-        visit[curr] = true;
-        for (int dest : graph[curr]) {
-            if (!visit[dest]) {
-                res[dest].push_back(parent);
-                dfs(graph, parent, dest, res, visit);
+    void dfs(int u,int ancestor,vector<vector<int>>& result,unordered_map<int,vector<int>>& mp){
+        for(int v : mp[u]){
+            if(result[v].empty() || result[v].back() != ancestor){
+                result[v].push_back(ancestor);
+                dfs(v,ancestor,result,mp);
             }
         }
+    }
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> result(n);
+        unordered_map<int,vector<int>> mp;
+
+        for(auto &vec : edges){
+            int u = vec[0];
+            int v = vec[1];
+
+            mp[u].push_back(v); // u-->v directed edge.
+        }
+
+        for(int i=0;i<n;i++){
+            //DFS
+            int ancestor = i;
+            dfs(i,ancestor,result,mp);
+        }
+        return result;
     }
 };
