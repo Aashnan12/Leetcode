4522004bbc0c735d8cbd0993af2b1e1eb38db331
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-07-09 10:14:03 +0530

    Time: 166 ms (28.82%) | Memory: 76.7 MB (35.00%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& arr) {
+        int waiting = 0;
+        double ans = 0;
+        int n = arr.size();
+
+        for(int i = 0; i<n ; i++){
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            if(waiting > start){
+                waiting += end;
+            }
+            else{
+                waiting = start + end;
+            }
+            ans += waiting - start;
+        }
+        cout<<ans << " "<< n;
+        return ans/n;
+    }
+};
