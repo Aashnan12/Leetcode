331ce7e2ad257af4f7e2176d629dd0be8f41a4c7
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-07-25 14:28:24 +0530

    Time: 246 ms (49.20%) | Memory: 142.1 MB (42.75%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,33 +1,79 @@
+//Merge sort
 class Solution {
 public:
-    int partition(vector<int>& nums,int s,int e){
+    void Merge(vector<int>& nums,int s,int mid,int e){
         int i = s;
-        int j = e;
-        int pivot = nums[s];
-        
-        while(i<j){
-            while(i<e && nums[i] <= pivot) i++;
-            while(j>s && nums[j] > pivot) j--;
+        int j = mid+1;
+        vector<int> arr(e-s+1);
+        int idx = 0;
 
-            if(i<j){
-                swap(nums[i],nums[j]);
+        while(i <= mid && j <= e){
+            if(nums[i] <= nums[j]){
+                arr[idx++] = nums[i++];
+            }
+            else{
+                arr[idx++] = nums[j++];
             }
         }
 
-        swap(nums[s],nums[j]);
-        return j;
+        while(i<=mid){
+            arr[idx++] = nums[i++];
+        }
+        while(j<=e){
+            arr[idx++] = nums[j++];
+        }
+        for(int a = s; a <= e; a++){
+            nums[a] = arr[a-s]; 
+        }
+        return;
     }
-    void QuickSort(vector<int>& nums,int s,int e){
+    void MergeSort(vector<int>& nums,int s,int e){
         if(s >= e){
             return;
         }
-        int j = partition(nums,s,e);
-        QuickSort(nums,s,j-1);
-        QuickSort(nums,j+1,e);
+        int mid = s + (e-s)/2;
+        MergeSort(nums,s,mid);
+        MergeSort(nums,mid+1,e);
+        Merge(nums,s,mid,e);
         return;
     }
     vector<int> sortArray(vector<int>& nums) {
-        QuickSort(nums,0,nums.size()-1);
+        MergeSort(nums,0,nums.size()-1);
         return nums;
     }
 };
+
+//Quick Sort
+// class Solution {
+// public:
+//     int partition(vector<int>& nums,int s,int e){
+//         int i = s;
+//         int j = e;
+//         int pivot = nums[s];
+        
+//         while(i<j){
+//             while(i<e && nums[i] <= pivot) i++;
+//             while(j>s && nums[j] > pivot) j--;
+
+//             if(i<j){
+//                 swap(nums[i],nums[j]);
+//             }
+//         }
+
+//         swap(nums[s],nums[j]);
+//         return j;
+//     }
+//     void QuickSort(vector<int>& nums,int s,int e){
+//         if(s >= e){
+//             return;
+//         }
+//         int j = partition(nums,s,e);
+//         QuickSort(nums,s,j-1);
+//         QuickSort(nums,j+1,e);
+//         return;
+//     }
+//     vector<int> sortArray(vector<int>& nums) {
+//         QuickSort(nums,0,nums.size()-1);
+//         return nums;
+//     }
+// };
