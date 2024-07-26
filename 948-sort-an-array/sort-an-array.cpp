753129552da5263adfc3360d6ef8cd63796ad236
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-07-26 09:19:40 +0530

    Time: 163 ms (55.43%) | Memory: 98.2 MB (46.04%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,52 +1,76 @@
-// Heap sort
+// Count Sort
 class Solution {
 public:
-    void BuildHeap(vector<int>& nums,int parent,int n){
-        int maxi = parent;
-        int c1 = parent*2 + 1;
-        int c2 = parent*2 + 2;
-
-        if(c1 < n && nums[maxi] < nums[c1]){
-            maxi = c1;
-        }
-        if(c2 < n && nums[maxi] < nums[c2]){
-            maxi = c2;
-        }
-
-        if(parent != maxi){
-            swap(nums[maxi],nums[parent]);
-            BuildHeap(nums,maxi,n);
-        }
-    }
-    void Heapify(vector<int>& nums,int n){
-        for(int i=n/2-1; i>=0 ;i--){
-            BuildHeap(nums,i,n);
-        }
-    }
-    void HeapSort(vector<int>& nums,int n){
-        Heapify(nums,n);
-        int size = n-1;
-        while(size > 0){
-            swap(nums[0],nums[size]);
-            BuildHeap(nums,0,size);
-            size--;
-        }
-    }
     vector<int> sortArray(vector<int>& nums) {
-        HeapSort(nums,nums.size());
+        unordered_map<int, int> freq;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+
+        for (int num : nums) {
+            freq[num]++;
+            maxi = max(maxi, num);
+            mini = min(mini, num);
+        }
+        int idx = 0;
+
+        for (int i = mini; i <= maxi; i++) {
+            while(freq[i] > 0){
+                nums[idx++] = i;
+                freq[i]--;
+            }
+        }
         return nums;
     }
 };
 
-
-//Merge sort
+// Heap sort
 // class Solution {
 // public:
-//     void Merge(vector<int>& nums,int s,int mid,int e){
-//         int i = s;
-//         int j = mid+1;
-//         vector<int> arr(e-s+1);
-//         int idx = 0;
+//     void BuildHeap(vector<int>& nums, int parent, int n) {
+//         int maxi = parent;
+//         int c1 = parent * 2 + 1;
+//         int c2 = parent * 2 + 2;
+
+//         if (c1 < n && nums[maxi] < nums[c1]) {
+//             maxi = c1;
+//         }
+//         if (c2 < n && nums[maxi] < nums[c2]) {
+//             maxi = c2;
+//         }
+
+//         if (parent != maxi) {
+//             swap(nums[maxi], nums[parent]);
+//             BuildHeap(nums, maxi, n);
+//         }
+//     }
+//     void Heapify(vector<int>& nums, int n) {
+//         for (int i = n / 2 - 1; i >= 0; i--) {
+//             BuildHeap(nums, i, n);
+//         }
+//     }
+//     void HeapSort(vector<int>& nums, int n) {
+//         Heapify(nums, n);
+//         int size = n - 1;
+//         while (size > 0) {
+//             swap(nums[0], nums[size]);
+//             BuildHeap(nums, 0, size);
+//             size--;
+//         }
+//     }
+//     vector<int> sortArray(vector<int>& nums) {
+//         HeapSort(nums, nums.size());
+//         return nums;
+//     }
+// };
+
+// Merge sort
+//  class Solution {
+//  public:
+//      void Merge(vector<int>& nums,int s,int mid,int e){
+//          int i = s;
+//          int j = mid+1;
+//          vector<int> arr(e-s+1);
+//          int idx = 0;
 
 //         while(i <= mid && j <= e){
 //             if(nums[i] <= nums[j]){
@@ -64,7 +88,7 @@ public:
 //             arr[idx++] = nums[j++];
 //         }
 //         for(int a = s; a <= e; a++){
-//             nums[a] = arr[a-s]; 
+//             nums[a] = arr[a-s];
 //         }
 //         return;
 //     }
@@ -84,14 +108,14 @@ public:
 //     }
 // };
 
-//Quick Sort
-// class Solution {
-// public:
-//     int partition(vector<int>& nums,int s,int e){
-//         int i = s;
-//         int j = e;
-//         int pivot = nums[s];
-        
+// Quick Sort
+//  class Solution {
+//  public:
+//      int partition(vector<int>& nums,int s,int e){
+//          int i = s;
+//          int j = e;
+//          int pivot = nums[s];
+
 //         while(i<j){
 //             while(i<e && nums[i] <= pivot) i++;
 //             while(j>s && nums[j] > pivot) j--;
