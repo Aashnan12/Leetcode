9b7dabc9528398c57a2a10087f9789f45e7c25a5
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-08-01 13:50:05 +0530

    Time: 4 ms (94.03%) | Memory: 14.7 MB (13.46%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -3,29 +3,47 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> A = nums; // good practice;
 
-        unordered_map<int,vector<int>> map; //for index mapping as index's will be lost after sort.
+        unordered_map<int,int> map; //for index mapping as index's will be lost after sort.
         int n = A.size();
         for(int i=0;i<n;i++){
-            map[A[i]].push_back(i);
-        }
-        sort(A.begin(),A.end());
-        int s = 0,e = n-1;
-
-        while(s<=e){
-            int sum = A[s] + A[e];
-            if(sum == target){
-                if(A[s] == A[e]){
-                    return {map[A[s]][0],map[A[e]][1]};
-                }
-                return {map[A[s]][0],map[A[e]][0]};
+            int num = target - nums[i];
+            if(map.find(num) != map.end()){
+                return {map[num],i};
             }
-            else if(sum < target){
-                s++;
-            }
-            else{
-                e--;
-            }
+            map[nums[i]] = i;
         }
         return {-1,-1};
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> A = nums; // good practice;
+
+//         unordered_map<int,vector<int>> map; //for index mapping as index's will be lost after sort.
+//         int n = A.size();
+//         for(int i=0;i<n;i++){
+//             map[A[i]].push_back(i);
+//         }
+//         sort(A.begin(),A.end());
+//         int s = 0,e = n-1;
+
+//         while(s<=e){
+//             int sum = A[s] + A[e];
+//             if(sum == target){
+//                 if(A[s] == A[e]){
+//                     return {map[A[s]][0],map[A[e]][1]};
+//                 }
+//                 return {map[A[s]][0],map[A[e]][0]};
+//             }
+//             else if(sum < target){
+//                 s++;
+//             }
+//             else{
+//                 e--;
+//             }
+//         }
+//         return {-1,-1};
+//     }
+// };
