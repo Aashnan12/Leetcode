ebb772adc709546b86e4b1779b1679d22a81943b
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-08-09 06:56:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (65.60%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         vector<int> freq(1001,0);
+        vector<int> ans;
+
         for(int num : arr1){
             freq[num]++;
         }
 
-        vector<int> ans;
         for(int num : arr2){
-            if(freq[num] > 0){
-                while(freq[num] > 0){
-                    ans.push_back(num);
-                    freq[num]--;
-                }
+            while(freq[num] > 0){
+                ans.push_back(num);
+                freq[num]--;
             }
         }
         for(int i=0;i<1001;i++){
