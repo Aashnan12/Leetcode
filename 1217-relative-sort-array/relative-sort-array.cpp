e6acfbf1e6c169232be624491e850001de720aca
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-08-09 07:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (5.23%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        vector<int> freq(1001,0);
-        vector<int> ans;
-
-        for(int num : arr1){
-            freq[num]++;
+        unordered_map<int,int> mp;
+        for(int i=0;i<arr2.size();i++){
+            mp[arr2[i]] = i;
         }
-
-        for(int num : arr2){
-            while(freq[num] > 0){
-                ans.push_back(num);
-                freq[num]--;
+        sort(arr1.begin(),arr1.end(),[&mp](int &a,int &b){
+            if(mp.count(a) && mp.count(b)){
+                return mp[a] < mp[b];
             }
-        }
-        for(int i=0;i<1001;i++){
-            while(freq[i] > 0){
-                ans.push_back(i);
-                freq[i]--;
+            else if(mp.count(a)){
+                return true;
             }
-        }
-        return ans;
+            else if(mp.count(b)){
+                return false;
+            }
+            else{
+                return a<b;
+            }
+        });
+        return arr1;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+//         vector<int> freq(1001,0);
+//         vector<int> ans;
+
+//         for(int num : arr1){
+//             freq[num]++;
+//         }
+
+//         for(int num : arr2){
+//             while(freq[num] > 0){
+//                 ans.push_back(num);
+//                 freq[num]--;
+//             }
+//         }
+//         for(int i=0;i<1001;i++){
+//             while(freq[i] > 0){
+//                 ans.push_back(i);
+//                 freq[i]--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
