9a2268c13b7f4f94835708ee4cfc6bdece03a610
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-09-23 11:24:09 +0530

    Time: 55 ms (31.61%) | Memory: 16.6 MB (15.63%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,32 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> lastseen(3,-1);
+        // vector<int> lastseen(3,-1);
+        // int cnt = 0;
+        // for(int i=0;i<s.size();i++){
+        //     lastseen[s[i] - 'a'] = i;
+        //     if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
+        //         cnt = cnt + (1 + min(lastseen[0],min(lastseen[1],lastseen[2])));
+        //     }
+        // }
+        // return cnt;
+
+        int left = 0;
+        int n = s.size();
+        int right = 0;
+        unordered_map<char,int> mp;
         int cnt = 0;
-        for(int i=0;i<s.size();i++){
-            lastseen[s[i] - 'a'] = i;
-            if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
-                cnt = cnt + (1 + min(lastseen[0],min(lastseen[1],lastseen[2])));
+        while(right < n){
+            mp[s[right]]++;
+            while(mp.size() == 3){
+                cnt += n - right;
+                mp[s[left]]--;
+                if(mp[s[left]] == 0) {
+                    mp.erase(s[left]);
+                }
+                left++;
             }
+            right++;
         }
         return cnt;
     }
