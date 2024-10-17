fa960b6bca7e75bc1695a581e3e40cb805dab8b2
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-10-17 11:16:10 +0530

    Time: 40 ms (35.24%) | Memory: 36.1 MB (85.73%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -5,37 +5,37 @@ public:
         int m = colSum.size();
         vector<vector<int>> mat(n,vector<int>(m,0));
 
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(rowSum[i] <= colSum[j]){
-        //             mat[i][j] = rowSum[i];
-        //             colSum[j] -= rowSum[i];
-        //             rowSum[i] = 0;
-        //         }
-        //         else{
-        //             mat[i][j] = colSum[j];
-        //             rowSum[i] -= colSum[j];
-        //             colSum[j] = 0;
-        //         }
-        //     }
-        // }
-
-
-
-        int i = 0, j = 0;
-
-        while(i<n && j<m){
-            mat[i][j] = min(rowSum[i],colSum[j]);
-            rowSum[i] -= mat[i][j];
-            colSum[j] -= mat[i][j];
-
-            if(rowSum[i] == 0){
-                i++;
-            }
-            else{
-                j++;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(rowSum[i] <= colSum[j]){
+                    mat[i][j] = rowSum[i];
+                    colSum[j] -= rowSum[i];
+                    rowSum[i] = 0;
+                }
+                else{
+                    mat[i][j] = colSum[j];
+                    rowSum[i] -= colSum[j];
+                    colSum[j] = 0;
+                }
             }
         }
+
+
+
+        // int i = 0, j = 0;
+
+        // while(i<n && j<m){
+        //     mat[i][j] = min(rowSum[i],colSum[j]);
+        //     rowSum[i] -= mat[i][j];
+        //     colSum[j] -= mat[i][j];
+
+        //     if(rowSum[i] == 0){
+        //         i++;
+        //     }
+        //     else{
+        //         j++;
+        //     }
+        // }
 
 
         return mat;
