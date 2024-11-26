3429982140cba0879369a5832eff5a86129446f4
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-11-26 10:58:14 +0530

    Time: 0 ms (100.00%) | Memory: 24.5 MB (25.96%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -1,13 +1,27 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums, int k) {
+//         int ans = 0;
+//         priority_queue<int, vector<int> , greater<int>> pq;
+//         for(int i=0;i<nums.size();i++){
+//             pq.push(nums[i]);
+//         }
+//         while(pq.top() < k){
+//             pq.pop();
+//             ans++;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
+        // Sort the array to process smallest elements first
+        sort(nums.begin(), nums.end());
+
         int ans = 0;
-        priority_queue<int, vector<int> , greater<int>> pq;
-        for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]);
-        }
-        while(pq.top() < k){
-            pq.pop();
+        for (int num : nums) {
+            if (num >= k) break; // Stop when reaching an element >= k
             ans++;
         }
         return ans;
