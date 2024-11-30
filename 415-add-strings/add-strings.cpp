e58295be7bc642e20bc60c8aa92540fb49f2030d
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-11-30 22:51:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (36.47%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     string addStrings(string a, string b) {
         string ans = "";
-        int i=a.size()-1, j=b.size()-1,sum=0;
+        int i = a.size()-1,j = b.size()-1 , sum = 0;
 
-        while(i>=0 || j>=0 || sum){ 
+        while(i>=0 || j>=0 || sum){
             if(i>=0){
-                sum += a[i--] - '0' ;
+                sum += a[i--] - '0';
             }
             if(j>=0){
                 sum += b[j--] - '0';
