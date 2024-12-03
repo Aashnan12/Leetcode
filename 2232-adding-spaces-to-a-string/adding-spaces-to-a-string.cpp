e605f43f57e6b74f4e1eba29c0f6dff9c78888ca
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2024-12-03 11:23:23 +0530

    Time: 11 ms (96.69%) | Memory: 82.3 MB (88.45%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,16 +1,36 @@
+// class Solution {
+// public:
+//     string addSpaces(string s, vector<int>& spaces) {
+//         int i = 0;
+//         string res = "";
+//         res.reserve(s.size() + spaces.size());
+//         for(int j = 0;j<s.size();j++){
+//             if(i<spaces.size() && spaces[i] == j){
+//                 res += " ";
+//                 i++;
+//             }
+//             res += s[j];
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        int i = 0;
-        string res = "";
-        res.reserve(s.size() + spaces.size());
-        for(int j = 0;j<s.size();j++){
-            if(i<spaces.size() && spaces[i] == j){
-                res += " ";
-                i++;
+        int n = s.size();
+        int m = spaces.size();
+        string res(n + m, ' '); // Pre-allocate the size for the result string
+        int i = 0, j = 0, k = 0;
+
+        while (j < n) {
+            if (i < m && spaces[i] == j) {
+                k++; // Move to the next position in `res` for the space
+                i++; // Move to the next space index
             }
-            res += s[j];
+            res[k++] = s[j++]; // Add the character from the original string
         }
+        
         return res;
     }
 };
