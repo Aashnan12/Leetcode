74a90eb0cdfd4f49395a13fa3a1619de7eedd365
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2025-02-04 14:22:58 +0530

    Time: 9 ms (33.37%) | Memory: 21 MB (16.76%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,56 +1,16 @@
-//Using DFS
-// class Solution {
-// public:
-//     bool isCycle(int u,unordered_map<int,vector<int>>& map,vector<bool>& vis,vector<bool>& inRecursion){
-//         vis[u] = true;
-//         inRecursion[u] = true;
-
-//         for(int &v : map[u]){
-//             if(!vis[v] && isCycle(v,map,vis,inRecursion)){
-//                 return true;
-//             }
-//             else if(inRecursion[v]){
-//                 return true;
-//             }
-//         }
-//         inRecursion[u] = false;
-//         return false;
-//     }
-//     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-//         unordered_map<int,vector<int>> map;
-//         for(vector<int>& v : prerequisites){
-//             map[v[1]].push_back(v[0]);
-//         }
-
-//         vector<bool> inRecursion(n+1,false);
-//         vector<bool> vis(n+1,false);
-
-//         for(int i=0;i<n;i++){
-//             if(!vis[i] && isCycle(i,map,vis,inRecursion)){
-//                 return false; // If cycle , cannot complete courses.
-//             }
-//         }
-//         return true;
-//     }
-// };
-
-
-// Using BFS
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> map;
-        for(vector<int>& v : prerequisites){
-            map[v[1]].push_back(v[0]);
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        unordered_map<int,vector<int>> mp;
+        for(auto p : pre){
+            mp[p[1]].push_back(p[0]);
         }
-
         vector<int> indegree(n+1,0);
         for(int i=0;i<n;i++){
-            for(int &u : map[i]){
+            for(int &u : mp[i]){
                 indegree[u]++;
             }
         }
-
         queue<int> q;
         for(int i=0;i<n;i++){
             if(indegree[i] == 0){
@@ -62,13 +22,13 @@ public:
             int v = q.front();
             q.pop();
             courses++;
-            for(int &u : map[v]){
+            for(int &u : mp[v]){
                 indegree[u]--;
                 if(indegree[u] == 0){
                     q.push(u);
                 }
             }
         }
-        return courses >= n;
+        return courses>=n;
     }
 };
