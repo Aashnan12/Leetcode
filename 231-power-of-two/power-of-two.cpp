a46f964451d513db350b1a26c7a61d8ab6b05b6c
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2025-02-19 20:47:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.08%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 && (n & (n-1))==0;
+        if(n==0)return 0;
+        return floor(log2(n)) == ceil(log2(n));
     }
 };
