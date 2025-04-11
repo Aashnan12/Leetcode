92444b405e9640c240f4ef6a78d18e52b706ac14
Aashnan <138841307+Aashnan12@users.noreply.github.com>
2025-04-11 12:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (36.07%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,49 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> A = nums; // good practice;
-
-        unordered_map<int,int> map; //for index mapping as index's will be lost after sort.
-        int n = A.size();
-        for(int i=0;i<n;i++){
-            int num = target - nums[i];
-            if(map.find(num) != map.end()){
-                return {map[num],i};
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(target - nums[i]) != mp.end()){
+                return {mp[target - nums[i]],i};
             }
-            map[nums[i]] = i;
+            mp[nums[i]] = i;
         }
-        return {-1,-1};
+        return {0,0};
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> A = nums; // good practice;
-
-//         unordered_map<int,vector<int>> map; //for index mapping as index's will be lost after sort.
-//         int n = A.size();
-//         for(int i=0;i<n;i++){
-//             map[A[i]].push_back(i);
-//         }
-//         sort(A.begin(),A.end());
-//         int s = 0,e = n-1;
-
-//         while(s<=e){
-//             int sum = A[s] + A[e];
-//             if(sum == target){
-//                 if(A[s] == A[e]){
-//                     return {map[A[s]][0],map[A[e]][1]};
-//                 }
-//                 return {map[A[s]][0],map[A[e]][0]};
-//             }
-//             else if(sum < target){
-//                 s++;
-//             }
-//             else{
-//                 e--;
-//             }
-//         }
-//         return {-1,-1};
-//     }
-// };
